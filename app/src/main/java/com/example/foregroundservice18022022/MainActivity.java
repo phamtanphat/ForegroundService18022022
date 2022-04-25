7180fa561a434769524f4cafe09c2ddd61d595bc
Phat <phatdroid94@gmail.com>
2022-04-25 19:19:30 +0700

    xu ly service

--- app/src/main/java/com/example/foregroundservice18022022/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice18022022/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,MyService.class);
+                intent.putExtra("text","Hello");
                 startService(intent);
             }
         });
@@ -32,5 +37,8 @@ public class MainActivity extends AppCompatActivity {
                 stopService(new Intent(MainActivity.this,MyService.class));
             }
         });
+
     }
+
+
 }
--- app/src/main/java/com/example/foregroundservice18022022/MyService.java
+++ app/src/main/java/com/example/foregroundservice18022022/MyService.java
@@ -1,13 +1,57 @@
 package com.example.foregroundservice18022022;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
+
+    private Looper serviceLooper;
+    private ServiceHandler serviceHandler;
+    private NotificationManager notificationManager;
+    private int isProgress;
+
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            // Normally we would do some work here, like download a file.
+            // For our sample, we just sleep for 5 seconds.
+            try {
+                long value = 0;
+                for (long i = 0; i < 5; i++) {
+                    Thread.sleep(5000);
+                    value += i;
+                }
+                Log.d("BBB",Thread.currentThread().getName());
+                makenoti(value+ "");
+                isProgress = 0;
+            } catch (InterruptedException e) {
+                // Restore interrupt status.
+                Thread.currentThread().interrupt();
+            }
+            // Stop the service using the startId, so that we don't stop
+            // the service in the middle of handling another job
+            stopSelf(msg.arg1);
+        }
+
+    }
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,19 +61,42 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("BBB","onCreate");
+        HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+
+        // Get the HandlerThread's Looper and use it for our Handler
+        serviceLooper = thread.getLooper();
+        serviceHandler = new ServiceHandler(serviceLooper);
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("BBB","onStartCommand");
-        return super.onStartCommand(intent, flags, startId);
+        if (isProgress == 0){
+            isProgress = 1;
+            Message msg = serviceHandler.obtainMessage();
+            msg.arg1 = startId;
+            serviceHandler.sendMessage(msg);
+        }
+        return START_REDELIVER_INTENT;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("BBB","onDestroy");
-
     }
+
+    public void makenoti(String message) {
+        Notification noti = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL_ID")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setWhen(System.currentTimeMillis())  //When the event occurred, now, since noti are stored by time.
+                .setContentTitle("Service")   //Title message top row.
+                .setContentText(message)  //message when looking at the notification, second row
+                .setAutoCancel(true)   //allow auto cancel when pressed.
+                .build();  //finally build and return a Notification.
+
+        //Show the notification
+        notificationManager.notify(1, noti);
+    }
+
 }
